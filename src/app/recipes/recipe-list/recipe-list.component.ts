0f7c2570e9abbfda63fe4b092b4d33a2baec22b9
Deschant Kounou <kounoudeschant@gmail.com>
2020-02-12 11:51:23 +0200

    Extract recipe item into own component

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -14,6 +14,12 @@ export class RecipeListComponent implements OnInit {
       'Just a test',
       'https://images.unsplash.com/photo-1540660290370-8aa90e451e8a?ixlib' +
       '=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80'
+    ),
+    new Recipe(
+      'A Test Recipe',
+      'Just a test',
+      'https://images.unsplash.com/photo-1540660290370-8aa90e451e8a?ixlib' +
+      '=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80'
     )
   ];
 
